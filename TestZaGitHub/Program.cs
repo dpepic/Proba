ccdb82c0660dd1afaca9ade81ac19f3d751c5010
Dragan Pepic <d.pepic@hotmail.com>
2019-10-18 16:16:42 +0200

    Add project files.

---
+++ TestZaGitHub/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestZaGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test za git hub");
+        }
+    }
+}
